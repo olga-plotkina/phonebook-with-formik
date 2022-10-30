587a6d7b4f08bb4bf25c4455ab45e87f11b59e96
olga-plotkina <olga.semenyuta@gmail.com>
2022-10-30 18:34:05 +0000

    delete extra lines

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 import { Formik, Field, ErrorMessage } from 'formik';
 
 import { FormBox } from './ContactForm.styled';
-// import { Component } from 'react';
+
 
 const schema = yup
   .object()
@@ -12,21 +12,13 @@ const initialValues = {
   number: '',
 };
 export const ContactForm = ({ submitProp }) => {
-  // handleChange = event => {
-  //   const { name, value } = event.currentTarget;
-  //   this.setState({ [name]: value });
-  // };
+
 
   const handleSubmit = (values, { resetForm }) => {
-    console.log(values);
-    resetForm();
     submitProp(values);
-    // this.reset();
+    resetForm();
   };
 
-  // reset = () => {
-  //   this.setState({ name: '', number: '' });
-  // };
 
   return (
     <Formik
@@ -40,11 +32,6 @@ export const ContactForm = ({ submitProp }) => {
           <Field
             type="text"
             name="name"
-            // value={this.state.name}
-            // onChange={this.handleChange}
-            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            // required
           />
           <ErrorMessage name="name" component="div" />
         </label>
@@ -53,9 +40,6 @@ export const ContactForm = ({ submitProp }) => {
           <Field
             type="tel"
             name="number"
-            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            // required
           />
           <ErrorMessage name="number" component="div" />
         </label>
